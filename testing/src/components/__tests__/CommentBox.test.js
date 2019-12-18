1d61eeec91efcf068ca1bd49f1abda55d95e18d6
Shubhangi Ambade <shubhambade6897@gmail.com>
2019-12-18 17:15:32 +0530

    Wrote test cases for the comment box files

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -8,7 +8,18 @@ Enzyme.configure({ adapter: new Adapter() });
 
 it('has a text area and button', () => {
     const wrapped = mount(<CommentBox />);
-
-    console.log(wrapped.find('textarea'));
+    console.log("Shubhangi", wrapped.find('textarea'));
     console.log(wrapped.find('button'));
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1)
+})
+
+it('has a textarea that users can type in', () => {
+    const wrapped = mount(<CommentBox />);
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 })
