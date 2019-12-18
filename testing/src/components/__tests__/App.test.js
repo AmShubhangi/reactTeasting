ef817864b95aa92e81cac4970b22d4f687395a90
Shubhangi Ambade <shubhambade6897@gmail.com>
2019-12-18 12:24:27 +0530

    chnages

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -5,13 +5,30 @@ import App from '../App';
 import CommentBox from '../CommentBox';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import CommentList from '../CommentList';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('shows a comment box', () => {
     const wrapped = shallow(<App />);
 
-    expect(wrapped.find(CommentBox).length);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+    // Tetsing
+    // const div = document.createElement('div');
+
+    // ReactDOM.render(<App />, div);
+    // // You will get this text in terminal
+    // console.log(div.innerHTML);
+    // expect(div.innerHTML).toContain('Comment Box');
+
+    // ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('shows a comment List', () => {
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentList).length).toEqual(1);
     // Tetsing
     // const div = document.createElement('div');
 
