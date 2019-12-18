15c858bb1b39a845104a5d62c96ba3f5d8c0ac78
Shubhangi Ambade <shubhambade6897@gmail.com>
2019-12-18 12:21:49 +0530

    Testing for comment box is done with enzyme

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('shows a comment box', () => {
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentBox).length);
     // Tetsing
-    const div = document.createElement('div');
+    // const div = document.createElement('div');
 
-    ReactDOM.render(<App />, div);
-    // You will get this text in terminal
-    console.log(div.innerHTML);
+    // ReactDOM.render(<App />, div);
+    // // You will get this text in terminal
+    // console.log(div.innerHTML);
+    // expect(div.innerHTML).toContain('Comment Box');
 
-    ReactDOM.unmountComponentAtNode(div);
+    // ReactDOM.unmountComponentAtNode(div);
 });
